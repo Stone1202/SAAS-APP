7fa5a7d4d51de77e96adedb53ae8c1b1d08d818b
Stone1202 <stone1202@users.noreply.github.com>
2026-08-11 19:15:11 +0800

    deploy: fix blank page - complete build with /SAAS-APP/ base

---
+++ assets/swimlanesDiagram-ULZ7WXOC-93_cNHL6.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-UKHOOZJN-Blp3IJrj.js";import{_ as a}from"./mermaid.core-DytBmuRE.js";import"./index-IrceO36l.js";import"./chunk-5VM5RSS4-D43GRhig.js";import"./chunk-XXDRQBXY-DcQWCnrW.js";import"./chunk-KBJHAD2P-D1HJlVoX.js";import"./chunk-2GRJ4B5K-DC6yxQuQ.js";import"./channel-B_JMAU3S.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
