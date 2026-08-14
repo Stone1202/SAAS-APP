2367ee37e94edc419754a5bc68ddde747c16f5b3
jianman1202 <jianman1202@163.com>
2026-08-14 17:28:30 +0800

    deploy: v3.1.52 build (app/admin/tenant) + PRD HTML v3.1.52

--- assets/swimlanesDiagram-ULZ7WXOC-BjQ0uyS0.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-UKHOOZJN-p0Oz_WSP.js";import{_ as a}from"./mermaid.core-m_4F7V-A.js";import"./index-BtPc9Txx.js";import"./chunk-5VM5RSS4-B-QfmWA6.js";import"./chunk-XXDRQBXY-CXXYCVot.js";import"./chunk-KBJHAD2P-DPnX5u1l.js";import"./chunk-2GRJ4B5K-cu9p7pA9.js";import"./channel-DrTgVUkZ.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
